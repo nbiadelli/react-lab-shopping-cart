2c077d514f7cc063c16ad3dee56b253b9a6a35a5
Natalia Biadelli <natalia.biadelli@gmail.com>
2019-02-01 20:31:25 -0200

    Foi criado os seguintes componentes: App.js (Componente princial/header)/Product.js(resumo de valores)

--- enjoei-web/src/App.js
+++ enjoei-web/src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo_header from './img/logo.png';
+import Product from './component/Product';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App-container">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <div class="logo">
+          <img src={logo_header} alt="logo"/>
+        </div>
         </header>
+        <body className="container">
+          <Product />
+        </body>
       </div>
     );
   }
---
+++ enjoei-web/src/component/Product.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import img_product from '../img/product.png'
+import '../css/Product.css';
+
+class Product extends Component {
+  render() {
+    return (
+        <div className="main">
+            <div className="img-product">
+                <img src={img_product} alt="imag"/>
+            </div> 
+            <div className="coupon-product">
+
+         
+            </div> 
+            <div className="cost-product">
+                <div className="cost-title">
+                   <ul>
+                       <li>resumo</li>
+                    </ul>
+                </div>
+                <div className="cost-label">
+                    <ul>
+                        <li>valor original</li>
+                        <li>cupom</li>
+                        <li>frete</li>
+                        <li>total</li>
+                    </ul>
+                </div>
+                <div className="cost-valule">
+                    <ul>
+                        <li>R$100,00</li>
+                        <li>R$100,00</li>
+                        <li>R$100,00</li>
+                        <li>R$100,00</li>
+                    </ul>
+                
+                </div>            
+            </div>            
+
+        </div>    
+    );
+  }
+}
+
+export default Product;
